be10ec2d98ca47f540561f9979fa2670bf871976
JuanjDes <juanjguapo@gmail.com>
2024-11-05 21:53:20 +0100

    Añadir elementos a una lista V 0.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
  // Aquí tu código
+
+const lista = document.getElementById('lista');
+const agregar = document.getElementById('agregar');
+const borrarElemento = document.getElementById('borrarElemento');
+const borrarLista = document.getElementById('borrarLista');
+
+agregar.addEventListener('click', function() {
+    const elemento = document.createElement('li');
+    elemento.textContent = prompt('Ingrese un elemento');
+    lista.appendChild(elemento);
+});
+
+borrarElemento.addEventListener('click', function() {
+    lista.removeChild(lista.lastChild);
+});
+
+borrarLista.addEventListener('click', function() {
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+});
